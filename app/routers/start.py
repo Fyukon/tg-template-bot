8a18d46fbf0664e7f4fadf3002a6d3c8241a4ecf
Алексей <olseky2610@mail.ru>
2025-12-25 18:23:52 +0300

    Added more buttons

--- app/routers/start.py
+++ app/routers/start.py
@@ -18,19 +18,19 @@ async def back_menu(callback: types.CallbackQuery):
 
 
 @router.callback_query(F.data == "services")
-async def back_menu(callback: types.CallbackQuery):
+async def service_menu(callback: types.CallbackQuery):
     await callback.message.edit_text(text="На данный момент вы выбираете услуги:", reply_markup=services_kb())
     await callback.answer()
 
 
 @router.callback_query(F.data == "request")
-async def back_menu(callback: types.CallbackQuery):
+async def request_menu(callback: types.CallbackQuery):
     await callback.message.edit_text(text="Отправьте свои контакты", reply_markup=services_kb())
     await callback.answer()
 
 
 @router.callback_query(F.data == "about")
-async def back_menu(callback: types.CallbackQuery):
+async def about_menu(callback: types.CallbackQuery):
     await callback.message.edit_text(text="Это просто тренировочный бот, тут может быть много всего, мало смысла",
                                      reply_markup=back_kb())
     await callback.answer()
