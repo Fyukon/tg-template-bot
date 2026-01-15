6da1fac48ac6dbc9fb7c26b501abdef274f68fea
Алексей <olseky2610@mail.ru>
2026-01-15 12:14:27 +0300

    Added alembic, but only test

--- app/main.py
+++ app/main.py
@@ -2,20 +2,21 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy import text
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from app.config import config
+from app.database import async_main
 from app.routers.common import router as router_common
 from app.routers.form import router as router_form
-from app.database import async_main
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+
 async def test_db_connection():
     try:
-        engine = create_async_engine(config.DATABASE_URL, echo = False)
+        engine = create_async_engine(config.DATABASE_URL, echo=False)
         async with engine.connect() as conn:
             await conn.execute(text("SELECT 1"))
         logger.info("Connection established")
@@ -24,8 +25,6 @@ async def test_db_connection():
     except Exception as e:
         logger.error(f"Не удалось подключиться к БД: {e}")
         exit(1)
-
-
 
 
 async def main():
